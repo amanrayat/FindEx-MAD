65dc3d5d3d934e95fe8cafedf88c8f064f806423
amanrayat <amanrayat32@gmail.com>
2019-07-24 14:58:16 -0400

    Adding login and register

--- app/src/main/java/com/example/findex/MainActivity.java
+++ app/src/main/java/com/example/findex/MainActivity.java
@@ -4,35 +4,73 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 // AKA Login page
 public class MainActivity extends AppCompatActivity {
 
     private Button login;
     private Button register;
+    private EditText userNameText , passwordText;
+    private FirebaseAuth authReference;
+    private String usernameData, passwordData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        userNameText = findViewById(R.id.loginEmail);
+        passwordText = findViewById(R.id.loginPassword);
+
+        authReference = FirebaseAuth.getInstance();
+
         // When "login" button is clicked, move to FoundList activity
         login = findViewById(R.id.button);
+
+        /*
+        Click listener for the login Button.
+         */
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openFoundList();
+                usernameData = userNameText.getText().toString();
+                passwordData = passwordText.getText().toString();
+                authenticateUserLogin();
             }
         });
 
-        // When "login" button is clicked, move to FoundList activity
         register = findViewById(R.id.register);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openRegistration();
+            }
+        });
+    }
+
+    /**
+     * This function is used to authenticate the user and change the intent.
+     */
+    private void authenticateUserLogin(){
+        authReference.signInWithEmailAndPassword(usernameData,passwordData).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    openFoundList();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Login Failed - Use correct Username / Password", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/findex/Registration.java
+++ app/src/main/java/com/example/findex/Registration.java
@@ -1,28 +1,59 @@
 package com.example.findex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Registration extends AppCompatActivity {
 
-    Button registration;
+    private Button registration;
+    private EditText userNameText , passwordText, confirmPasswordText;
+    private FirebaseAuth authReference;
+    private String usernameData, passwordData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        userNameText = findViewById(R.id.registerEmail);
+        passwordText = findViewById(R.id.registerPassword);
+        confirmPasswordText = findViewById(R.id.registerPassword);
 
-
+        authReference = FirebaseAuth.getInstance();
         registration = findViewById(R.id.buttonRegistration);
         registration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openFoundList();
+                usernameData = userNameText.getText().toString();
+                passwordData = passwordText.getText().toString();
+                registerUser();
+            }
+        });
+    }
+
+    private void registerUser() {
+        authReference.createUserWithEmailAndPassword(usernameData, passwordData).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Registration Success", Toast.LENGTH_LONG).show();
+                    openFoundList();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Registration is Unsuccessful", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
